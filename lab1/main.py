50fd650f01b84e242df1f1ec7e3eec84d2795d7e
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-09-27 23:12:03 +0200

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -19,6 +19,7 @@ train.fillna(train.mean(numeric_only=True), inplace=True)
 test.fillna(test.mean(numeric_only=True), inplace=True)
 
 #TESTING
+'''
 print("***** Train_Set *****")
 print(train.head())
 print("\n")
@@ -29,7 +30,7 @@ print("\n")
 print("***** Train_Set *****")
 print(train.describe())
 print(train.columns.values)
-
+'''
 
 # For the train set
 train.isna().head()
@@ -37,12 +38,50 @@ train.isna().head()
 test.isna().head()
 
 #the total number of missing values in both datasets
+'''
 print("*****In the train set*****")
 print(train.isna().sum())
 print("\n")
 print("*****In the test set*****")
 print(test.isna().sum())
+'''
 
-print(train['Ticket'].head())
-print(train['Cabin'].head())
+#print(train['Ticket'].head())
+#print(train['Cabin'].head())
+
+"""
+print("\n*****Pclass*****")
 print(train[['Pclass','Survived']].groupby(['Pclass'],as_index=False).mean(). sort_values(by='Survived', ascending=False))
+
+print("\n*****Sex*****")
+print(train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived',ascending=False))
+
+print("\n*****Sibsp*****")
+print(train[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived',ascending=False))
+"""
+
+#quick-plotting: "Age vs Survived"
+"""
+g = sns.FacetGrid(train, col='Survived') 
+g.map(plt.hist, 'Age', bins=20) 
+plt.show()
+
+#how the Pclass and Survived features are related to each other with a graph:
+grid = sns.FacetGrid(train, col='Survived', row='Pclass', aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+grid.add_legend()
+plt.show()
+"""
+
+train = train.drop(['Name','Ticket', 'Cabin','Embarked'], axis=1)
+test = test.drop(['Name','Ticket', 'Cabin','Embarked'], axis=1)
+
+labelEncoder = LabelEncoder()
+labelEncoder.fit(train['Sex'])
+labelEncoder.fit(test['Sex'])
+train['Sex'] = labelEncoder.transform(train['Sex']) 
+test['Sex'] = labelEncoder.transform(test['Sex'])
+
+train.info()
+test.info()
+
